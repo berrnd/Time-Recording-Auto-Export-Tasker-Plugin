b8f3115e163bf0104f45c9bc74b76e352b0ab084
Bernd Bestel <bernd@berrnd.de>
2015-04-19 14:26:26 +0200

    Use studio64.exe instead of studio.exe

--- _OpenInAndroidStudio.bat
+++ _OpenInAndroidStudio.bat
@@ -5,4 +5,4 @@ set androidStudioBin=A:\AndroidStudio\bin
 rem Android Studio needs the project path without a trailing backslash
 if %projectPath:~-1%==\ set projectPath=%projectPath:~0,-1%
 
-start "" /D "%androidStudioBin%" "studio.exe" "%projectPath%"
+start "" /D "%androidStudioBin%" "studio64.exe" "%projectPath%"
